585c92366cb9bb340407d02e32d87e4701039ba4
Demons <liujp@cloud-young.com>
2018-08-24 15:20:08 +0800

    设置日期

--- src-example/components/new-calendar.vue
+++ src-example/components/new-calendar.vue
@@ -2,14 +2,13 @@
     <div class="new-calendar-wrapper">
       <div class="calendar-flex">
         <div class="flex-row" v-for="calendarDay in calendarDays">
-          <div class="day-one" v-for="day in calendarDay">{{day.day}}</div>
+          <div class="day-one" v-for="everyDay in calendarDay">{{everyDay.day}}</div>
         </div>
       </div>
     </div>
 </template>
 <script>
     /* eslint-disable */
-    import test from '../../src/components/test.vue'
     export default {
       data() {
         return {
@@ -22,6 +21,39 @@
             [ {"day":1,"disabled":false,'lunar':"23" }, {"day":2,"disabled":false,'lunar':"23"}, {"day":3,"disabled":false,'lunar':"23"}, {"day":4,"disabled":false,'lunar':"23"} , {"day":5,"disabled":false,'lunar':"23"}, {"day":6,"disabled":false,'lunar':"23"}, {"day":0,"disabled":false,'lunar':"23"} ]
          ]
         }
+      },
+      methods: {
+        render () {
+          let allDays = []; // 总数据
+          let column = 0;  // 第一行数据
+          let now = new Date();
+          let year = now.getFullYear(); // 当前年
+          let month = now.getMonth();   // 当前月
+          let day = now.getDate();       // 当前日期
+          let firstDayOfNowMonth = new Date(year, month, 1).getDay();  // 当月第一天 星期几
+          let alllastDateOfNowMonth = new Date(year, month, 0).getDate() // 上月总天数(上月最后一天日期)
+          let allDateOfNowMonth = new Date(year, month + 1, 0).getDate() // 当月总天数
+          for(var i=1;i<allDateOfNowMonth+1;i++) {
+            // 循环每一天
+            var nowDay = new Date(year, month, i).getDay();    // 当前星期几
+            if (nowDay == 6) {
+              // 每个周末创建一个数组
+              column++
+              allDays[column] = []
+            } else if(i == 1) {
+              //nowDay是当前index
+              // 第一天时, 创建第一行数组,当前周几,并补全前面的几天 29 index->0 30 index->1 31 index->2  1号 index->3  2号index->4
+              allDays[column] = []
+              for(var j=0;j<nowDay;j++) {
+                  allDays[column].push({"day":alllastDateOfNowMonth--,"disabled":false,'lunar':"23" })
+              }
+            }
+          }
+          console.log(allDays)
+        }
+      },
+      created() {
+        this.render();
       }
     }
 </script>
